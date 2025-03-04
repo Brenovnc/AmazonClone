dcce0a1005a55f60d15285ccba6eb874c5c09ab4
Breno Vieira <brenovnc435@gmail.com>
2025-03-04 18:10:52 -0300

    removendo o HTML linkado ao botão

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -22,7 +22,7 @@ listCartItems.innerHTML = list
 
 function addList(product, quantity) {
     return `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-conteiner-${product.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -108,5 +108,8 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click', () => {
             const productId = link.dataset.productId
             removeFromCart(productId);
+            
+            const container = document.querySelector(`.js-cart-item-conteiner-${productId}`)
+            container.remove()
         })
     })
