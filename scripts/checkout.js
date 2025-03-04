dfa390f32b1749d043c6a7f0f4fbd525f0c8ecd3
Breno Vieira <brenovnc435@gmail.com>
2025-03-04 17:40:30 -0300

    corrijindo bugs e modularizando o código

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/cart.js'
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 const listCartItems = document.querySelector(".js-order-summary"); 
 let list = ''
@@ -37,7 +38,7 @@ function addList(product, quantity) {
                     ${product.name}
                 </div>
                 <div class="product-price">
-                    ${(product.priceCents/100).toFixed(2)}
+                    ${formatCurrency(product.priceCents)}
                 </div>
                 <div class="product-quantity">
                 <span>
@@ -59,7 +60,7 @@ function addList(product, quantity) {
                 <div class="delivery-option">
                 <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                 <div>
                     <div class="delivery-option-date">
                     Tuesday, June 21
@@ -72,7 +73,7 @@ function addList(product, quantity) {
                 <div class="delivery-option">
                 <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                 <div>
                     <div class="delivery-option-date">
                     Wednesday, June 15
@@ -85,7 +86,7 @@ function addList(product, quantity) {
                 <div class="delivery-option">
                 <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                 <div>
                     <div class="delivery-option-date">
                     Monday, June 13
